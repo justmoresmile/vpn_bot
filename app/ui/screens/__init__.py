21451dc7780846168128678f375d5708476aa486
justmoresmile <justmoresmile@yandex.ru>
2026-07-27 17:56:13 +0300

    move support screen to separate file

--- app/ui/screens/__init__.py
+++ app/ui/screens/__init__.py
@@ -4,31 +4,36 @@ from .admin import (
     admin_user_screen,
 )
 
+
 from .subscription import (
     subscription_screen,
     no_subscription_screen,
     my_vpn_screen,
 )
 
+
 from .payment import (
     payment_screen,
     payment_success_screen,
+    payment_renew_success_screen,
 )
+
 
 from .buy import (
     buy_screen,
 )
 
+
 from .profile import (
     profile_screen,
 )
 
+
 from .support import (
-    
     support_screen,
 )
+
 
 from .instruction import (
     instruction_screen,
 )
-
